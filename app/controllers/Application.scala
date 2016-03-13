49cc3608fa98dee082299215d8fdeca462221539
pedrorijo91 <pedrorijo91@gmail.com>
2016-03-13 14:06:22 +0000

    Initial version

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,75 +1,126 @@
 package controllers
 
+import java.net.URLEncoder
+import models.{UrbanResponse, UrbanResponseResultType}
+import play.api.Logger
 import play.api.Play.current
-import play.api.libs.functional.syntax._
-import play.api.libs.json.{Reads, _}
 import play.api.libs.ws.{WS, WSResponse}
 import play.api.mvc._
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.{Failure, Success, Try}
 
-/*
-{
-  "tags":["what","wut","wtf","wats","lol","fuck","the","you","huh","u"],
-  "result_type":"exact", # no_results
-  "list":[
-    {
-      "definition":"The only proper response to something that makes absolutely no sense.",
-      "permalink":"http://wat.urbanup.com/3322419",
-      "thumbs_up":3159,
-      "author":"watwat",
-      "word":"wat",
-      "defid":3322419,
-      "current_vote":"",
-      "example":"1: If all the animals on the equator were capable of flattery, Halloween and Easter would fall on the same day.\r\n2: wat\r\n\r\n1: Wow your cock is almost as big as my dad's.\r\n2: wat\r\n\r\n1: I accidentially a whole coke bottle\r\n2: You accidentially what?\r\n1: A whole coke bottle\r\n2: wat",
-      "thumbs_down":370
-    }
-  ],
-  "sounds":["http://media.urbandictionary.com/sound/wat-8530.mp3","http://media.urbandictionary.com/sound/wat-8531.mp3"]
- }
- */
-case class UrbanResponse(resultType: String, list: List[UrbanTerm])
-
-object UrbanResponse {
-
-  implicit val userReads: Reads[UrbanResponse] = (
-    (__ \ "result_type").read[String] and
-      (__ \ "list").read[List[UrbanTerm]]
-    ) (UrbanResponse.apply _)
-}
-
-case class UrbanTerm(definition: String, permalink: String, thumbsUp: Int, thumbsDown: Int, author: String, example: String)
-
-object UrbanTerm {
- // implicit val fmt: Format[UrbanTerm] = Json.format[UrbanTerm]
-
-  implicit val userReads: Reads[UrbanTerm] = (
-    (__ \ "definition").read[String] and
-    (__ \ "permalink").read[String] and
-    (__ \ "thumbs_up").read[Int] and
-    (__ \ "thumbs_down").read[Int] and
-    (__ \ "author").read[String] and
-    (__ \ "example").read[String]
-    ) (UrbanTerm.apply _)
-}
 
 object Application extends Controller {
 
   def index = Action {
-
-    val term="wat"
-    val url = s"https://mashape-community-urban-dictionary.p.mashape.com/define?term=$term"
-    val mashapeKey = "zVGRvBkMxvmshNSgfXLth7VqUejlp1TdkV5jsnBvqfAiVw75Yu"
-
-    val result: WSResponse = Await.result(WS.url(url).withHeaders(("X-Mashape-Key", mashapeKey)).get(), 2 seconds) //TODO 2 seconds
-
-    val valid: JsResult[UrbanResponse] = result.json.validate[UrbanResponse]
-
-    println(s"VALIDATE: ${valid}")
-
-    Ok(views.html.index(valid))
+    Ok("urbanSlackPlay")
   }
 
+  def urban = Action {
+    request =>
+
+      request.body.asFormUrlEncoded.fold {
+        Logger.error(s"Problem on Slack request, no params were found on request: $request")
+        BadRequest("Problem on Slack request")
+      } {
+        keyValueMap =>
+
+          val terms = keyValueMap.getOrElse("text", Seq.empty) //FIXME empty
+
+          val term = terms.mkString(" ")
+          val encodedTerm = URLEncoder.encode(terms.mkString(" "), "UTF-8")
+
+          val url = s"https://mashape-community-urban-dictionary.p.mashape.com/define?term=$encodedTerm"
+          val mashapeKey = "zVGRvBkMxvmshNSgfXLth7VqUejlp1TdkV5jsnBvqfAiVw75Yu" // TODO config
+
+          Try {
+            val result: WSResponse = Await.result(WS.url(url).withHeaders(("X-Mashape-Key", mashapeKey)).get(), 4 seconds)
+
+            val response = result.json.validate[UrbanResponse].asEither
+
+            response match {
+              case Left(e) =>
+                Logger.error(s"Problem validating API response: $e")
+                InternalServerError("A error was found. Please try again")
+              case Right(urbanResponse) if urbanResponse.resultType == UrbanResponseResultType.Unknown =>
+                Logger.error(s"Unexpected resultType for $term")
+                InternalServerError("Unexpected response returned. Please try again")
+              case Right(urbanResponse) if urbanResponse.resultType == UrbanResponseResultType.NoResults =>
+                Logger.info(s"No results: ${urbanResponse.definitions}")
+                NotFound(s"No definitions were found to '$term' on Urban Dictionary")
+              case Right(urbanResponse) =>
+                Logger.info(s"Success response: ${urbanResponse.resultType} => ${urbanResponse.definitions.size}")
+                val message = createResponse(term, urbanResponse)
+                Ok(message)
+            }
+          } match {
+            case Failure(ex) =>
+              Logger.error(s"Exception on API request: $ex")
+              RequestTimeout("Some network problem was found. Please try again")
+            case Success(r) =>
+              r
+          }
+      }
+  }
+
+  private def createResponse(term: String, urbanResponse: UrbanResponse): String = {
+
+    val message = urbanResponse.definitions.headOption.map {
+      definition =>
+        s"""${definition.definition} - by ${definition.author} (${definition.thumbsUp} up / ${definition.thumbsDown} down). More at ${definition.permalink}"""
+    }.getOrElse("Not found on Urban Dictionary")
+
+    s"'$term': $message"
+  }
+
+
+  def prettyUrban = Action {
+    request =>
+
+      request.body.asFormUrlEncoded.fold {
+        Logger.error(s"Problem on Slack request, no params were found on request: $request")
+        BadRequest("Problem on Slack request")
+      } {
+        keyValueMap =>
+
+          val terms = keyValueMap.getOrElse("text", Seq.empty) //FIXME empty
+
+          val term = terms.mkString(" ")
+          val encodedTerm = URLEncoder.encode(terms.mkString(" "), "UTF-8")
+
+          val url = s"https://mashape-community-urban-dictionary.p.mashape.com/define?term=$encodedTerm"
+          val mashapeKey = "zVGRvBkMxvmshNSgfXLth7VqUejlp1TdkV5jsnBvqfAiVw75Yu" // TODO config
+
+          Try {
+            val result: WSResponse = Await.result(WS.url(url).withHeaders(("X-Mashape-Key", mashapeKey)).get(), 4 seconds)
+
+            val response = result.json.validate[UrbanResponse].asEither
+
+            response match {
+              case Left(e) =>
+                Logger.error(s"Problem validating API response: $e")
+                InternalServerError("A error was found. Please try again")
+              case Right(urbanResponse) if urbanResponse.resultType == UrbanResponseResultType.Unknown =>
+                Logger.error(s"Unexpected resultType for $term")
+                InternalServerError("Unexpected response returned. Please try again")
+              case Right(urbanResponse) if urbanResponse.resultType == UrbanResponseResultType.NoResults =>
+                Logger.info(s"No results: ${urbanResponse.definitions}")
+                NotFound(s"No definitions were found to '$term' on Urban Dictionary")
+              case Right(urbanResponse) =>
+                Logger.info(s"Success response: ${urbanResponse.resultType} => ${urbanResponse.definitions.size}")
+                val message = createResponse(term, urbanResponse)
+                Ok(message)
+            }
+          } match {
+            case Failure(ex) =>
+              Logger.error(s"Exception on API request: $ex")
+              RequestTimeout("Some network problem was found. Please try again")
+            case Success(r) =>
+              r
+          }
+      }
+  }
 
 }
